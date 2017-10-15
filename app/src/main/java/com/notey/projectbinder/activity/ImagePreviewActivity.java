d21b2ed42c3772094ba9a2b3f26952cff9a1ec56
Ben Holmes <holben888@gmail.com>
2017-10-15 07:42:31 -0400

    Alert dialog when done taking picture. Other important changes to fix bugs with camera.

--- app/src/main/java/com/notey/projectbinder/activity/ImagePreviewActivity.java
+++ app/src/main/java/com/notey/projectbinder/activity/ImagePreviewActivity.java
@@ -1,15 +1,20 @@
 package com.notey.projectbinder.activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Parcel;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -54,9 +59,6 @@ public class ImagePreviewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 System.out.println(bmp);
                 storeImage(bmp);
-                //bmp.writeToParcel(parcel, 0);
-                //parcel.setDataPosition(0);
-
             }
         });
     }
@@ -71,9 +73,13 @@ public class ImagePreviewActivity extends AppCompatActivity {
             FileOutputStream fos = new FileOutputStream(pictureFile);
             image.compress(Bitmap.CompressFormat.PNG, 90, fos);
             fos.close();
-            CreateNewNoteTask.ImageData i = new CreateNewNoteTask.ImageData(pictureFile.getPath(), "aaaa", "image/png");
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Calendar cal = Calendar.getInstance();
+            String date = dateFormat.format(cal);
+            CreateNewNoteTask.ImageData i = new CreateNewNoteTask.ImageData(pictureFile.getPath(), date, "image/png");
             System.out.println(i.getPath());
-            new CreateNewNoteTask("aaaaaaa", "bbbbbbbbbbb", i, null, null).start(ImagePreviewActivity.this);
+            new CreateNewNoteTask(date, "", i, null, null).start(ImagePreviewActivity.this);
+            presentSuccessAlert();
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         } catch (IOException e) {
@@ -102,5 +108,29 @@ public class ImagePreviewActivity extends AppCompatActivity {
         String mImageName="MI_.jpg";
         mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
         return mediaFile;
+    }
+
+    public void presentSuccessAlert() {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(ImagePreviewActivity.this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(ImagePreviewActivity.this);
+        }
+        builder.setTitle("All Set!")
+                .setMessage("Your note was uploaded successfully")
+                .setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent(ImagePreviewActivity.this, MainActivity.class);
+                        startActivity(i);
+                    }
+                })
+                .setNegativeButton(R.string.take_another, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setIcon(0)
+                .show();
     }
 }
